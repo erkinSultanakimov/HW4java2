81368c9e133470a1a2e6ab1e2101f02c230f69e9
erkinSultanakimov <erkin.sultanakimov@gmail.com>
2021-04-12 19:33:01 +0600

    Исправил

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,47 +8,33 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> listA = new ArrayList<>();
         System.out.println("Enter words for listA:");
-        listA.add(scanner.next());
-        listA.add(scanner.next());
-        listA.add(scanner.next());
-        listA.add(scanner.next());
-        listA.add(scanner.next());
-
-        Iterator<String> iterator = listA.iterator();
-        System.out.println("listA------------");
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
+        for (int i = 0; i < 5; i++) {
+            listA.add(scanner.next());
         }
+        Print(listA);
+
 
         ArrayList<String> listB = new ArrayList<>();
         System.out.println("Enter words for listB:");
-        listB.add(scanner.next());
-        listB.add(scanner.next());
-        listB.add(scanner.next());
-        listB.add(scanner.next());
-        listB.add(scanner.next());
+        for (int i = 0; i < 5; i++) {
+            listB.add(scanner.next());
+
+        }
+        Print(listB);
+
         Collections.reverse(listB);
 
-        Iterator<String> iterator2 = listB.iterator();
-        System.out.println("listB------------");
-        while (iterator2.hasNext()){
-            System.out.println(iterator2.next());
-        }
 
         ArrayList<String> listC = new ArrayList<>();
         System.out.println("listC");
 
+        for (int i = 0; i <5 ; i++) {
+            listC.add(listA.get(i));
+            listC.add(listB.get(i));
 
-        listC.add(0,listA.get(0) );
-        listC.add(1 ,listB.get(0));
-        listC.add(2,listA.get(1));
-        listC.add(3 ,listB.get(1));
-        listC.add(4,listA.get(2));
-        listC.add(5 ,listB.get(2));
-        listC.add(6,listA.get(3));
-        listC.add(7 ,listB.get(3));
-        listC.add(8,listA.get(4));
-        listC.add(9,listB.get(4));
+        }
+        Print(listC);
+
         System.out.println(listC);
         Comparator<String> bylength = new Comparator<String>() {
             @Override
@@ -60,17 +46,16 @@ public class Main {
         System.out.println(listC);
 
 
+    }
 
-
-
-
-
-
-
-
+    public static void Print(ArrayList<String> list) {
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
 
     }
 
 
-
 }
+
